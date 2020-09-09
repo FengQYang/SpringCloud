751f4e7dab48001df5ed0a1c6c24c46b7a5e2534
78520 <1468416647@qq.com>
2020-09-09 14:54:10 +0800

    第二次提交

---
+++ SpringCloud-pojo/src/main/java/com/fqy/pojo/Book.java
@@ -0,0 +1,19 @@
+package com.fqy.pojo;
+
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class Book {
+
+    private Integer id;
+    private String cover;
+    private String title;
+    private String author;
+    private Date date;
+    private String press;
+    private String abs;
+    private Integer cid;
+}
---
+++ SpringCloud-shopfind/src/main/java/com/fqy/dao/BookDao.java
@@ -0,0 +1,11 @@
+package com.fqy.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface BookDao {
+
+
+}
